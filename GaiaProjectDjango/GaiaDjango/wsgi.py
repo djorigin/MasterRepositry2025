a25ce097f6741f3b23c2e1791ae47f122e0c40e2
DJmaster <dc5spa@hotmail.com>
2025-06-10 23:55:00 +0000

    creation of Django project

---
+++ GaiaProjectDjango/GaiaDjango/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GaiaDjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GaiaDjango.settings')
+
+application = get_wsgi_application()
