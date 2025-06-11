babe1a616b8df33372008bbaf6f8e28b58f670b0
DJmaster <dc5spa@hotmail.com>
2025-06-11 00:10:12 +0000

    Added systemcore to Gaia Project

--- GaiaProjectDjango/GaiaDjango/settings.py
+++ GaiaProjectDjango/GaiaDjango/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'systemcore',  # Custom app for system core functionalities
 ]
 
 MIDDLEWARE = [
